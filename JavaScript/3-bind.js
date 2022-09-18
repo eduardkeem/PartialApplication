b0f992443f6c97133cf2a9bbcf6dcee679284e57
Eduard <eduard.keem@gmail.com>
2022-09-18 22:59:42 +0600

    18.9.22

--- JavaScript/3-bind.js
+++ JavaScript/3-bind.js
@@ -6,7 +6,7 @@ const log = (base, n) => ln(n) / ln(base);
 
 // Usage
 {
-  const lg = log.bind(null, 10);
+  const lg = log.bind(null, 10);            // bind принимает обязательно 2 параметра, поэтому если передаем 1 параметр, тогда вместо второго передаем null
   const ln = log.bind(null, Math.E);
 
   console.log('lg(5) = ' + lg(5));
